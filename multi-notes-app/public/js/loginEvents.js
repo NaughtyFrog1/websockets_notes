389df2f73aa279f5b19dfad0abc4c07bfe76e998
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-06-23 10:52:01 -0300

    add join event

--- multi-notes-app/public/js/loginEvents.js
+++ multi-notes-app/public/js/loginEvents.js
@@ -1,4 +1,4 @@
-import { roomCreate } from './socket.js'
+import { roomCreate, roomJoin } from './socket.js'
 import { formatUuidInput, renderErrors } from './ui.js'
 
 const loginInputUsername = document.querySelector('#loginUsername')
@@ -6,8 +6,8 @@ const loginInputRoomId = document.querySelector('#loginRoomId')
 const loginBtnConnect = document.querySelector('#loginBtnConnect')
 const loginBtnCreate = document.querySelector('#loginBtnCreate')
 
-function validateUsername(input) {
-  const username = input.value.trim().replace(/\s+/g, ' ')
+function validateUsername(value) {
+  const username = value.trim().replace(/\s+/g, ' ')
   const errors = []
   if (username === '') errors.push('Username is required')
   else if (username.length < 2) errors.push('Username is too short')
@@ -15,15 +15,15 @@ function validateUsername(input) {
   return { data: username, errors }
 }
 
-function validateRoomId(input) {
+function validateRoomId(value) {
   const errors = []
-  if (input.value === '') errors.push('Room ID is required')
-  else if (input.value !== 36) errors.push('Room ID is incorrect')
-  return { data: input.value, errors }
+  if (value === '') errors.push('Room ID is required')
+  else if (value.length !== 36) errors.push('Room ID is incorrect')
+  return { data: value, errors }
 }
 
 function handleInputUsernameBlur(e) {
-  const { data, errors } = validateUsername(e.target)
+  const { data, errors } = validateUsername(e.target.value)
   renderErrors(errors, document.querySelector('#loginUsernameErrors'))
   e.target.value = data
 }
@@ -34,13 +34,27 @@ function handleInputRoomIdBlur(e) {
 }
 
 function handleBtnCreateClick() {
-  const { data, errors } = validateUsername(loginInputUsername)
+  const { data, errors } = validateUsername(loginInputUsername.value)
   renderErrors(errors, document.querySelector('#loginUsernameErrors'))
 
   if (errors.length > 0) return
 
   loginBtnCreate.disabled = true
+  loginBtnConnect.disabled = true
   roomCreate(data)
+}
+
+function handleBtnConnectClick() {
+  const username = validateUsername(loginInputUsername.value)
+  const roomId = validateRoomId(loginInputRoomId.value)
+  renderErrors(username.errors, document.querySelector('#loginUsernameErrors'))
+  renderErrors(roomId.errors, document.querySelector('#loginRoomIdErrors'))
+
+  if (username.errors.length > 0 || roomId.errors.length > 0) return
+
+  loginBtnCreate.disabled = true
+  loginBtnConnect.disabled = true
+  roomJoin(roomId.data, username.data)
 }
 
 export function addLoginEvents() {
@@ -48,6 +62,7 @@ export function addLoginEvents() {
   loginInputRoomId.addEventListener('keypress', formatUuidInput)
   loginInputRoomId.addEventListener('blur', handleInputRoomIdBlur)
   loginBtnCreate.addEventListener('click', handleBtnCreateClick)
+  loginBtnConnect.addEventListener('click', handleBtnConnectClick)
 }
 
 export function removeLoginEvents() {
--- multi-notes-app/public/js/socket.js
+++ multi-notes-app/public/js/socket.js
@@ -7,5 +7,14 @@ export function roomCreate(username) {
   socket.emit('room:create', username, (roomUUID, room) => {
     removeLoginEvents()
     showNotesSection(roomUUID)
+    renderNotes(room.notes)
+  })
+}
+
+export function roomJoin(uuid, username) {
+  socket.emit('room:join', uuid, username, (roomUUID, room) => {
+    removeLoginEvents()
+    showNotesSection(roomUUID)
+    renderNotes(room.notes)
   })
 }
